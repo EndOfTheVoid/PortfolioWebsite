ec3010c7d76c874e3e283c93fca7fe3344195b2f
EndOfTheVoid <148165378+EndOfTheVoid@users.noreply.github.com>
2023-10-20 04:29:17 +0530

    Add files via upload

---
+++ src/data/seo.js
@@ -0,0 +1,38 @@
+const SEO = [
+	{
+		page: "home",
+		description:
+			"I'm a final year student at IIT Kharagpur with working knowledge of many different fields. I've tried to implement that knowledge in real world solutions via some of my projects and internships.",
+		keywords: ["Atharva", "Atharva N", "Atharva Nigoskar"],
+	},
+
+	{
+		page: "about",
+		description:
+			"I'm a final year student at IIT Kharagpur with working knowledge of many different fields. I've tried to implement that knowledge in real world solutions via some of my projects and internships.",
+		keywords: ["Atharva", "Atharva N", "Atharva Nigoskar"],
+	},
+
+	// {
+	// 	page: "articles",
+	// 	description:
+	// 		"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	// 	keywords: ["Atharva", "Atharva N", "Atharva Nayanajith"],
+	// },
+
+	{
+		page: "projects",
+		description:
+			"I've done projects in a few different fields. Please feel free to take a look at them. Any and all feedback is appreciated.",
+		keywords: ["Atharva", "Atharva N", "Atharva Nigoskar"],
+	},
+
+	{
+		page: "contact",
+		description:
+			"If you're interested in collaborating on a project, or have an opportunity that you think I will be a good fit for, feel free to reach out to me. I'm always open to new ideas and opportunities.",
+		keywords: ["Atharva", "Atharva N", "Atharva Nigoskar"],
+	},
+];
+
+export default SEO;
---
+++ src/data/user.js
@@ -0,0 +1,78 @@
+const INFO = {
+	main: {
+		title: "Portfolio Website - Atharva",
+		name: "Atharva Nigoskar",
+		email: "atharva.nigoskar2@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		file: "https://drive.google.com/file/d/1jjFvtRPCD5wePQ0P5C-h2EVgOhsu6Gid/view?usp=sharing",
+		github: "https://github.com/EndOfTheVoid",
+		linkedin: "https://linkedin.com/in/atharva-nigoskar/",
+		instagram: "https://www.instagram.com/atharva.boi/",
+	},
+
+	homepage: {
+		title: "Jack of multiple trades, trying to decide which one to master.",
+		description:
+			"I am studying fullstack development with the MERN Stack. I have done a few projects to experience building web applications using various frameworks and technologies. I dabble in data science as well, with a special interest in Generative Models. I am also a finance enthusiast and will be appearing for CFA Level 1 examination this February. I enjoy solving complex problems and learning new skills. I did mention that I'm a Jack of Multiple Trades, didn't I 😉",
+	},
+
+	about: {
+		title: "I’m Atharva Nigoskar. I'm a student at IIT Kharagpur, where I'm trying to explore as much as I can.",
+		description:
+			"I've worked on a variety of competitions, self-projects and internships over my college life and I'm proud of the progress I've made. I've linked most of these in the projects section and highlighted others in my CV. If you have something you think I would be a good fit for, please reach out to my via any of the channels linked here. I'm always open to new ideas and feedback.",
+	},
+
+
+
+	projects: [
+		{
+			title: "Portfolio Website",
+			description:
+				"Created a professional portfolio website using ReactJS, demonstrating my learnings in making interactive websites in front-end development",
+			logo: "react.png",
+			linkText: "View Project",
+			link: "https://github.com/EndOfTheVoid/PortfolioWebsite",
+		},
+
+		{
+			title: "Graph Analysis and Hate Speech Detection",
+			description:
+				"Performed hate speech detection on 5000+ tweets using vectorization methods and classifiers like random forest, SVM and supervised FastText.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com/EndOfTheVoid/GraphAnalysis-HateSpeechDetection",
+		},
+
+		{
+			title: "To-Do List",
+			description:
+				"A simple yet effective To-do List I made to display my learnings of web development concepts, especially backend-development.",
+			logo: "mongo.jpg",
+			linkText: "View Project",
+			link: "https://github.com/EndOfTheVoid/To-Do-List",
+		},
+
+		// {
+		// 	title: "Project 4",
+		// 	description:
+		// 		"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+		// 	logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+		// 	linkText: "View Project",
+		// 	link: "https://github.com",
+		// },
+
+		// {
+		// 	title: "Project 5",
+		// 	description:
+		// 		"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+		// 	logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+		// 	linkText: "View Project",
+		// 	link: "https://github.com",
+		// },
+	],
+};
+
+export default INFO;
